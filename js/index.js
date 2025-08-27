38a731aa29db59b5098fc21a2ba224cad57c4d3d
Pritom678 <152401917+Pritom678@users.noreply.github.com>
2025-08-27 12:25:53 +0600

    fifth commit

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ getElement('call-box').addEventListener('click', function(e){
         getElement('heart-count').innerText = currentHeart;
     }
 })
+
+getElement('call-box').addEventListener('click', function(e){
+    if(e.target.closest('.btn-call')){
+        const callButton = e.target.closest('.btn-call');
+
+        const cardTitle = callButton.parentNode.parentNode.children[0].children[0].innerText;
+        const cardNumber = callButton.parentNode.parentNode.children[0].children[1].children[0].innerText;
+        
+        const coin = getElement('coin-count').innerText;
+
+        if(coin <= 0){
+            alert('Not enough coin' + ' Call terminated');
+            return;
+        }
+
+        alert(cardTitle + ' ' + cardNumber + ' calling....')
+
+        const currentCoin = Number(coin) - 20;
+        getElement('coin-count').innerText = currentCoin;
+
+        
+    }
+})
